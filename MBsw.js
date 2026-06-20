a607568533013c9b114ea646c8f4e31da024da97
edugrowth5-rgb <edugrowth5@gmail.com>
2026-06-20 21:23:58 +0530

    Refactor service worker caching logic

--- MBsw.js
+++ MBsw.js
@@ -1,7 +1,20 @@
-const CACHE_NAME = 'success-zone-v1';
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['index.html', 'MBstyle.css', 'MBscript.js', 'MBdata.js'])));
+const CACHE_NAME = "success-zone-v1";
+const ASSETS = [
+  "index.html",
+  "MBstyle.css",
+  "MBscript.js",
+  "MBdata1.js",
+  "MBchapter.js",
+  "MBquiz1.js",
+  "icon.png"
+];
+
+// Install Event
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+// Fetch Event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
